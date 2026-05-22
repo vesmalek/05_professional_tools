858f3176171332f3860324b3d4971c51fc080b52
Ismail Malik <vesmalick@gmail.com>
2026-05-22 23:03:36 +0300

    Practiced Python decorators

--- decorators_practice.py
+++ decorators_practice.py
@@ -1,16 +1,23 @@
 import time
 
 def timer_dec(base_fn):
-    def enhanced_fn():
+    def enhanced_fn(*args, **kwargs):
         start_time = time.time()
-        base_fn()
+        base_fn(*args, **kwargs)
         end_time = time.time()
         print(f'Task time: {(end_time - start_time):.4f}')
     return enhanced_fn
 
 @timer_dec
-def greetings():
-    print("Hello my friend, how're you?")
+def greetings(username, age):
+    time.sleep(1)
+    print(f"Hello my {username}, how're you? You're {age} years old")
 
-greetings()
+@timer_dec
+def travel_abroad():
+    time.sleep(1)
+    print(f"I am traveling abroad ...")
+
+greetings('izzy', 20)
+travel_abroad()
 
